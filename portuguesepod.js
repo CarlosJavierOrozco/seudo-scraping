940b8f71e2b7c535175135b33188784433213e12
Carlos J. Orozco <carlosjorozco.jw@gmail.com>
2024-06-12 17:22:36 -0400

    commit inicial

---
+++ portuguesepod.js
@@ -0,0 +1,109 @@
+//Elementos que contiene el PODCAST
+let a = document.querySelector(".r101-headline__intro > button:nth-child(1)")
+let b = document.querySelector("#download-center > ul:nth-child(1) > li:nth-child(1) > a:nth-child(1)")
+let dialog3 = document.querySelector("div.lsn3-lesson-audio:nth-child(2) > div:nth-child(1) > div:nth-child(1) > audio:nth-child(2)")
+
+
+//Elementos que contiene el dialogo
+let c = document.querySelector("#download-center > ul:nth-child(1) > li:nth-child(2) > a:nth-child(1)")
+
+
+//Elementos que contiene la  review
+let d = document.querySelector("#download-center > ul:nth-child(1) > li:nth-child(3) > a:nth-child(1)")
+
+//Elementos que contiene el audio de cada frase
+//primera frase
+let frase1 = document.querySelector("#dialogue_panel_00 > table:nth-child(1) > tbody:nth-child(1) > tr:nth-child(1) > td:nth-child(1) > div:nth-child(1) > button:nth-child(1)")
+let frase1a = document.querySelector("#dialogue_panel_00 > table:nth-child(1) > tbody:nth-child(1) > tr:nth-child(1) > td:nth-child(1) > div:nth-child(2) > button:nth-child(1)")
+
+//segunda frase
+let frase2 = document.querySelector("#dialogue_panel_00 > table:nth-child(1) > tbody:nth-child(1) > tr:nth-child(2) > td:nth-child(1) > div:nth-child(1) > button:nth-child(1)")
+let frase2a = document.querySelector("#dialogue_panel_00 > table:nth-child(1) > tbody:nth-child(1) > tr:nth-child(2) > td:nth-child(1) > div:nth-child(2) > button:nth-child(1)")
+
+//tercera frase
+let frase3 = document.querySelector("#dialogue_panel_00 > table:nth-child(1) > tbody:nth-child(1) > tr:nth-child(3) > td:nth-child(1) > div:nth-child(1) > button:nth-child(1)")
+let frase3a = document.querySelector("#dialogue_panel_00 > table:nth-child(1) > tbody:nth-child(1) > tr:nth-child(3) > td:nth-child(1) > div:nth-child(2) > button:nth-child(1)")
+
+//cuarta frase
+let frase4 = document.querySelector("#dialogue_panel_00 > table:nth-child(1) > tbody:nth-child(1) > tr:nth-child(4) > td:nth-child(1) > div:nth-child(1) > button:nth-child(1)")
+let frase4a = document.querySelector("#dialogue_panel_00 > table:nth-child(1) > tbody:nth-child(1) > tr:nth-child(4) > td:nth-child(1) > div:nth-child(2) > button:nth-child(1)")
+
+//quinta frase
+let frase5 = document.querySelector("#dialogue_panel_00 > table:nth-child(1) > tbody:nth-child(1) > tr:nth-child(5) > td:nth-child(1) > div:nth-child(1) > button:nth-child(1)")
+let frase5a = document.querySelector("#dialogue_panel_00 > table:nth-child(1) > tbody:nth-child(1) > tr:nth-child(5) > td:nth-child(1) > div:nth-child(2) > button:nth-child(1)")
+
+//sexta frase
+let frase6 = document.querySelector("#dialogue_panel_00 > table:nth-child(1) > tbody:nth-child(1) > tr:nth-child(6) > td:nth-child(1) > div:nth-child(1) > button:nth-child(1)")
+let frase6a = document.querySelector("#dialogue_panel_00 > table:nth-child(1) > tbody:nth-child(1) > tr:nth-child(6) > td:nth-child(1) > div:nth-child(2) > button:nth-child(1)")
+
+//en esta leccion tiene un segundo dialogo, estos son los elementos de ese segundo dialogo
+
+let frase7 = document.querySelector("#dialogue_panel_10 > table:nth-child(1) > tbody:nth-child(1) > tr:nth-child(1) > td:nth-child(1) > div:nth-child(2) > button:nth-child(1)")
+let frase8 = document.querySelector("#dialogue_panel_10 > table:nth-child(1) > tbody:nth-child(1) > tr:nth-child(2) > td:nth-child(1) > div:nth-child(2) > button:nth-child(1)")
+let frase9 = document.querySelector("#dialogue_panel_10 > table:nth-child(1) > tbody:nth-child(1) > tr:nth-child(3) > td:nth-child(2) > button:nth-child(1)")
+let frase10 = document.querySelector("#dialogue_panel_10 > table:nth-child(1) > tbody:nth-child(1) > tr:nth-child(4) > td:nth-child(2) > button:nth-child(1)")
+let frase11 = document.querySelector("#dialogue_panel_10 > table:nth-child(1) > tbody:nth-child(1) > tr:nth-child(5) > td:nth-child(2) > button:nth-child(1)")
+let frase12 = document.querySelector("#dialogue_panel_10 > table:nth-child(1) > tbody:nth-child(1) > tr:nth-child(6) > td:nth-child(2) > button:nth-child(1)")
+
+
+let f7 = document.querySelector("#dialogue_panel_10 > table:nth-child(1) > tbody:nth-child(1) > tr:nth-child(1) > td:nth-child(2) > button:nth-child(1)")
+let f8 = document.querySelector("#dialogue_panel_10 > table:nth-child(1) > tbody:nth-child(1) > tr:nth-child(2) > td:nth-child(2) > button:nth-child(1)")
+
+
+
+//vocabulario
+
+let vocab1 = document.querySelector(".lsn3-lesson-vocabulary > div:nth-child(1) > section:nth-child(1) > table:nth-child(1) > tbody:nth-child(1) > tr:nth-child(2) > td:nth-child(2) > div:nth-child(2) > button:nth-child(1)")
+
+let vocab2 = document.querySelector(".lsn3-lesson-vocabulary > div:nth-child(1) > section:nth-child(1) > table:nth-child(1) > tbody:nth-child(1) > tr:nth-child(3) > td:nth-child(2) > div:nth-child(2) > button:nth-child(1)")
+
+let vocab3 = document.querySelector(".lsn3-lesson-vocabulary > div:nth-child(1) > section:nth-child(1) > table:nth-child(1) > tbody:nth-child(1) > tr:nth-child(4) > td:nth-child(2) > div:nth-child(2) > button:nth-child(1)")
+
+let vocab4 = document.querySelector(".lsn3-lesson-vocabulary > div:nth-child(1) > section:nth-child(1) > table:nth-child(1) > tbody:nth-child(1) > tr:nth-child(5) > td:nth-child(2) > div:nth-child(2) > button:nth-child(1)")
+
+let vocab5 = document.querySelector(".lsn3-lesson-vocabulary > div:nth-child(1) > section:nth-child(1) > table:nth-child(1) > tbody:nth-child(1) > tr:nth-child(6) > td:nth-child(2) > div:nth-child(2) > button:nth-child(1)")
+
+let vocab6 = document.querySelector(".lsn3-lesson-vocabulary > div:nth-child(1) > section:nth-child(1) > table:nth-child(1) > tbody:nth-child(1) > tr:nth-child(7) > td:nth-child(2) > div:nth-child(2) > button:nth-child(1)")
+
+
+//URL del podcast
+let urla = a.dataset.url
+let urlb = b.href
+let dialog3Url = dialog3.dataset.trackurl     
+
+//URL del dialogo
+let urlc = c.href
+
+//URL de la review
+let urld = d.href
+
+
+//URLs de las frases
+let fraseA = frase1.dataset.src
+let fraseA1 = frase1a.dataset.url
+
+//textos de las frases
+let text1 = frase1a.dataset.text
+let textEng = frase1a.dataset.englishText
+//texto frase 2
+let text2 = frase2a.dataset.text
+let text2Eng = frase2a.dataset.englishText
+//texto frase 3
+let text3 = frase3a.dataset.text
+let text3Eng = frase3a.dataset.englishText
+//texto frase 4
+let text4 = frase4a.dataset.text
+let text4Eng = frase4a.dataset.englishText
+//texto frase 5
+let text5 = frase5a.dataset.text
+let text5Eng = frase5a.dataset.englishText
+
+
+
+
+
+
+
+
+
+
